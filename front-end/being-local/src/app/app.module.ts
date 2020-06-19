25d349a4f008f808d144211df6f1ee9ab847df9d
Binesh Narayanan <kbinesh@gmail.com>
2020-06-19 22:09:23 +0800

    landing and routing

--- front-end/being-local/src/app/app.module.ts
+++ front-end/being-local/src/app/app.module.ts
@@ -3,18 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTreeModule} from '@angular/material/tree';
+import {MatTreeModule, MatSliderModule, MatMenu, MatButton, MatMenuTrigger, MatMenuModule} from '@angular/material';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MissionComponent } from './mission/mission.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    MissionComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTreeModule
+    MatTreeModule,
+    MatSliderModule,
+    MatMenuModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
