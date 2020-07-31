38f1dbd9019b944402c31a20080ffc4c77cc8df7
Binesh Narayanan <kbinesh@gmail.com>
2020-07-31 17:58:35 +0800

    about-us refinement, card-component

--- front-end/being-local/src/app/modules/about-us/about-us.module.ts
+++ front-end/being-local/src/app/modules/about-us/about-us.module.ts
@@ -5,10 +5,12 @@ import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutUsComponent } from './about-us.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatDividerModule } from '@angular/material/divider';
+import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component';
 import { SearchBoxComponent } from 'src/app/shared/components/search-box/search-box.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [AboutUsComponent],
-  imports: [CommonModule, AboutUsRoutingModule, FlexLayoutModule]
+  imports: [CommonModule, AboutUsRoutingModule, FlexLayoutModule, SharedModule]
 })
 export class AboutUsModule {}
