1bff85c3b42d3c56b1b3ef161f69917068f1edb7
Binesh Narayanan <kbinesh@gmail.com>
2020-08-04 23:04:41 +0800

    adding country list and icons

---
+++ front-end/being-local/src/app/shared/components/country-list/country-list.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-list',
+  templateUrl: './country-list.component.html',
+  styleUrls: ['./country-list.component.scss']
+})
+export class CountryListComponent implements OnInit {
+  constructor() {}
+
+  countries: any;
+
+  ngOnInit(): void {
+    this.countries = [
+      {
+        id: '1',
+        name: 'India',
+        icon: 'country-india'
+      },
+      {
+        id: '2',
+        name: 'Japan',
+        icon: 'country-japan'
+      },
+      {
+        id: '3',
+        name: 'China',
+        icon: 'country-china'
+      },
+      {
+        id: '4',
+        name: 'Taiwan',
+        icon: 'country-taiwan'
+      },
+      {
+        id: '4',
+        name: 'South Korea',
+        icon: 'country-south-korea'
+      },
+      {
+        id: '4',
+        name: 'USA',
+        icon: 'country-usa'
+      },
+      {
+        id: '4',
+        name: 'Finland',
+        icon: 'country-finland'
+      },
+      {
+        id: '4',
+        name: 'Singapore',
+        icon: 'country-singapore'
+      }
+    ];
+  }
+}
