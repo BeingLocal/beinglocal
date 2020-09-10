5cb3eb22de8e34e4366ac704eeef30acc35e33fd
Binesh Narayanan <kbinesh@gmail.com>
2020-09-10 15:33:33 +0800

    integrating browse-by country API

--- front-end/being-local/src/app/app-routing.module.ts
+++ front-end/being-local/src/app/app-routing.module.ts
@@ -32,6 +32,13 @@ const routes: Routes = [
           import('./modules/brand-list/brand-list.module').then(
             m => m.BrandListModule
           )
+      },
+      {
+        path: 'browse-by/:category/:id/brands',
+        loadChildren: () =>
+          import('./modules/brand-list/brand-list.module').then(
+            m => m.BrandListModule
+          )
       }
     ]
   },
